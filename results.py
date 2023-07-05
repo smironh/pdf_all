9b6013648b76ee7fdb831be835d6aa2b174a675c
BEnZ <71022332+smironh@users.noreply.github.com>
2023-07-05 12:36:05 +0500

    Добавил pdf>> docx и pdf>>html

--- results.py
+++ results.py
@@ -2,6 +2,9 @@ from pdfplumber import PDF
 from pathlib import Path
 from gtts import gTTS
 
+from docx import Document
+from docx.shared import Inches
+
 import os
 
 def pdf_to_txt(file_name):
@@ -36,4 +39,60 @@ def pdf_to_mp3(file_path='test.pdf', language='en'):
 	file_name = Path(file_path).stem
 	my_audio.save(f'result/{file_name}.mp3')
 
-	return '[+] Готово!'
+	print('[+] Готово')
+
+def pdf_to_html(file_path):
+	print('[+] Файл принят')
+	print('[+] Конвертируем...')
+
+	with PDF(open(file=file_path, mode='rb')) as pdf:
+		pages = [page.extract_text() for page in pdf.pages]
+
+	text = ''.join(pages)
+	a = text.split('\n')
+	b = 0
+
+	file_name =  Path(file_path).stem
+	with open(f'result/{file_name}.html', 'w', encoding='utf-8') as file:
+		file.write('''
+<style>
+	body{
+	font-family: Arial;
+	}
+</style>
+''')
+	file = open(f'result/{file_name}.html', 'w', encoding='utf-8')
+	for i in range(len(a)):
+		txt = a[b]
+		txt = f'<p>{txt}</p>'
+		file.write(txt)
+		b+=1
+	file.close()
+
+	print('[+] Готово!')
+
+def pdf_to_docx(file_path):
+	print('[+] Файл принят')
+	print('[+] Конвертируем...')
+
+	with PDF(open(file=file_path, mode='rb')) as pdf:
+		pages = [page.extract_text() for page in pdf.pages]
+
+
+	file_name =  Path(file_path).stem	
+
+	text = ''.join(pages)
+	a = text.split('\n')
+	b = 0
+
+	document = Document()
+	document.add_heading(file_name, 0)
+
+	for i in range(len(a)):
+		txt = a[b]
+		txt = f'{txt}'
+		document.add_paragraph(txt)
+		b+=1
+
+	document.save(f'result/{file_name}.docx')
+	print('[+] Готово!')
