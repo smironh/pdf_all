d02ad7f2d4e1801a7ee28a5aa151689f06f200fc
BEnZ <71022332+smironh@users.noreply.github.com>
2023-07-05 12:37:03 +0500

    Обновление
    Добавил возможность конвертировать в html и docx
    Исправил ошибки

--- main.py
+++ main.py
@@ -8,9 +8,11 @@ def main():
 	print('''
 1)pdf >> txt
 2)pdf >> mp3
+3)pdf >> html
+4)pdf >> docx
 ''')
 
-	all_convert = [1, 2]
+	all_convert = [1, 2, 3, 4]
 
 	number = input('Введите что хотите запустить > ')
 	if int(number) in all_convert:
@@ -20,12 +22,26 @@ def main():
 
 				results.pdf_to_txt(path)
 			else:
-				print('Ошибка! Не найдет путь к .pdf файлу!')
+				print('Ошибка! Не найден путь к .pdf файлу!')
 		elif int(number) == 2:
-			language = input("Выберете язык, пример: 'en' или 'ru': ")
-			while language not in ("en", "ru"):
-				language = input("Выберете язык, пример: 'en' или 'ru':  ")
-			print(results.pdf_to_mp3(file_path=path, language=language))
+			if Path(path).is_file() and Path(path).suffix == '.pdf':
+
+				language = input("Выберете язык, пример: 'en' или 'ru': ")
+				while language not in ("en", "ru"):
+					language = input("Выберете язык, пример: 'en' или 'ru':  ")
+				print(results.pdf_to_mp3(file_path=path, language=language))
+			else:
+				print('Ошибка! Не найден путь к .pdf файлу!')
+		elif int(number) == 3:
+			if Path(path).is_file() and Path(path).suffix == '.pdf':
+				results.pdf_to_html(path)
+			else:
+				print('Ошибка! Не найден путь к .pdf файлу!')
+		elif int(number) == 4:
+			if Path(path).is_file() and Path(path).suffix == '.pdf':
+				results.pdf_to_docx(path)
+			else:
+				print('Ошибка! Не найден путь к .pdf файлу!')
 		else:
 			print('Ошибка! такой функции нет!')
 	else:
